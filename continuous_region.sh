738cf9e7f3996bdb8b97aea3f27651568c9b3869
Atlas Khan <akhan@phoenix.local>
2017-06-28 19:43:20 -0400

    continuous_region.sh

---
+++ continuous_region.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in *_aln_sorted.bam
+do
+samtools depth  $i | awk '{if ($3>=5) print $0}' | awk '{ if ($2!=(ploc+1)) {if (ploc!=0){printf("%s %d-%d\n",$1,s,ploc);}s=$2} ploc=$2; }' > continou_region.txt
+done
