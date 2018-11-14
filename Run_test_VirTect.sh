82804a2ac00986009f40c173cdac2690684c542e
Atlas Khan <atlas.akhan@gmail.com>
2018-11-14 17:06:56 -0500

    Update Run_test_VirTect.sh

--- Run_test_VirTect.sh
+++ Run_test_VirTect.sh
@@ -1,2 +1,2 @@
 ###############
-python VirTect.py -t 12 -1 ../VirTect-master/VirTect_Test_1.fq  -2 ../VirTect-master/VirTect_Test_1.fq   -o TEST_annika  -ucsc_gene ../UK_test/VirTect/human_reference/gencode.v29.annotation.gtf  -index ../UK_test/VirTect/human_reference/GRCh38.p12.genome -index_vir viruses_reference/viruses_757.fasta -d 200
+python VirTect.py -t 12 -1 Test_dataset/VirTect_Test_1.fq  -2 Test_dataset/VirTect_Test_1.fq   -o TEST  -ucsc_gene human_reference/gencode.v29.annotation.gtf  -index human_reference/GRCh38.p12.genome -index_vir viruses_reference/viruses_757.fasta -d 200
